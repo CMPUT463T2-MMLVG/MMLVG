6184562a92d52a9d9434a1462fefd57e79e4f9e9
Shelly Yuqing Xue <shellyxue99@icloud.com>
2020-11-18 20:14:05 -0500

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -3,56 +3,59 @@ import os
 import glob
 from PIL import Image
 
-#Load sprites
+# Load sprites
 sprites = {}
-for filename in glob.glob("./sprites/*.png"):
-	im = Image.open(filename)
-	splits = filename.split("/")
-	name = splits[-1][:-4]
-	sprites[name] = im
+for filename in glob.glob("./tiles/*.png"):
+    im = Image.open(filename)
+    splits = filename.split("/")
+    name = splits[-1][:-4]
+    sprites[name] = im
 
 visualization = {}
-visualization["S"] = "brick"
-visualization["?"] = "questionMark"
-visualization["Q"] = "emptyBlock"
-visualization["E"] = "goomba"
-visualization["<"] = "topLeftPipe"
-visualization[">"] = "topRightPipe"
-visualization["["] = "leftPipe"
-visualization["]"] = "rightPipe"
-visualization["o"] = "coin"
-visualization["B"] = "cannonTop"
-visualization["b"] = "cannonBottom"
+visualization["@"] = "null"
+visualization["#"] = "solid"
+visualization["-"] = "empty"
+visualization["B"] = "super_arm_block"
+visualization["H"] = "spikes"
+visualization["|"] = "ladder"
+visualization["L"] = "large_health"
+visualization["l"] = "small_health"
+visualization["W"] = "large_weapon_energy"
+visualization["w"] = "small_weapon_energy"
+visualization["+"] = "extra_life"
+visualization["M"] = "moving_platform"
+visualization["P"] = "player"
+visualization["C"] = "shooter"
+visualization["D"] = "boss_door"
+visualization["U"] = "beam"
+visualization["t"] = "undefined"
+visualization["*"] = "special_item"
 
-#Visualize Output Level
+# Visualize Output Level
 level = {}
-with open("./Generated Levels/output.txt") as fp:
-	y = 0
-	for line in fp:
-		level[y] = line
-		y+=1
+# file path
+with open("./TestFile.txt") as fp:
+    y = 0
+    for line in fp:
+        level[y] = line
+        y += 1
 
-image = Image.new("RGB", (1600, 240), color=(91, 153, 254))
+image = Image.new("RGB", (3000, 3000), color=(81, 50, 8))
 pixels = image.load()
 
-maxY = 15
-maxX = 100
+maxY = y
+maxX = len(line)
 
 for y in range(0, maxY):
-	for x in range(0, maxX):
-		imageToUse = None
-		if level[y][x] in visualization.keys():
-			imageToUse = sprites[visualization[level[y][x]]]
-		elif level[y][x]=="X":
-			if y>maxY-2:
-				imageToUse = sprites["ground"]
-			else:
-				imageToUse = sprites["stair"]
-		if not imageToUse == None:
-			pixelsToUse = imageToUse.load()
-			for x2 in range(0, 16):
-				for y2 in range(0, 16):
-					if pixelsToUse[x2,y2][3]>0:
-						pixels[x*16+x2,y*16+y2] = pixelsToUse[x2,y2][0:-1]
+    for x in range(0, maxX):
+        imageToUse = None
+        if level[y][x] in visualization.keys():
+            imageToUse = sprites[visualization[level[y][x]]]
+        if not imageToUse == None:
+            pixelsToUse = imageToUse.load()
+            for x2 in range(0, 16):
+                for y2 in range(0, 16):
+                    if pixelsToUse[x2, y2][3] > 0:
+                        pixels[x * 16 + x2, y * 16 + y2] = pixelsToUse[x2, y2][0:-1]
 
 image.save("output.jpeg", "JPEG")
